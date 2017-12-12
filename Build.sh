1da84eca45733b8b6be075bc5a3ca44f4dbdf8a1
Haider Zahoor <haider2090@gmail.com>
2017-12-12 08:01:00 +0500

    X556 Build #2
    Support for X556 build kernel.

--- Build.sh
+++ Build.sh
@@ -14,7 +14,7 @@ export ARCH=arm64 ARCH_MTK_PLATFORM=mt6735
 export SUBARCH=arm64
 
 #Defconfig for Moto C
-make -C $PWD O=$PWD/out ARCH=arm64 wt6737m_65_n_defconfig
+make -C $PWD O=$PWD/out ARCH=arm 64 rlk6737m_open_n_defconfig
 #make ARCH=arm64 wt6737m65_n_defconfig
 
 make -j4 -C $PWD O=$PWD/out ARCH=arm64
